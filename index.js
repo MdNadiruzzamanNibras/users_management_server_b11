85e16a80ad96aa8743ac6bc2eb36810e8122d2d3
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2026-02-20 12:52:05 +0600

    post method user

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -38,6 +41,14 @@ const users = [
     email: "sophia.martinez@example.com",
   },
 ];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("data asbe post method");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
